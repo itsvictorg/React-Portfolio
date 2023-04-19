6866a35f15f8a87fd1407b6c2cb709434ee3c3e9
unknown <itsvictorg@gmail.com>
2023-04-18 22:49:01 -0500

    useState to fix navbar not collapsing on mobile

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,25 +5,30 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 // import NavDropdown from 'react-bootstrap/NavDropdown'
 
+import { useState } from "react";
+
 import "../index.css";
 
 function MyNavbar() {
+
+    const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar sticky="top" bg="light" expand="lg" expanded={expanded}>
         
       <Container>
         
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")}  />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className='m-auto'>
           
-            <Nav.Link href="#/" className="nav-link">HOME</Nav.Link>
+            <Nav.Link href="#/" onClick={() => setExpanded(false)} className="nav-link">HOME</Nav.Link>
 
-            <Nav.Link href="#/about" className="nav-link">ABOUT</Nav.Link>
+            <Nav.Link href="#/about" onClick={() => setExpanded(false)} className="nav-link">ABOUT</Nav.Link>
 
-            <Nav.Link href="#/projects" className="nav-link">PROJECTS</Nav.Link>
+            <Nav.Link href="#/projects" onClick={() => setExpanded(false)} className="nav-link">PROJECTS</Nav.Link>
 
-            <Nav.Link href="#/contact" className="nav-link">CONTACT</Nav.Link>
+            <Nav.Link href="#/contact" onClick={() => setExpanded(false)} className="nav-link">CONTACT</Nav.Link>
 
             <Nav.Link href="https://drive.google.com/file/d/1IEeKGaZJlcc0Hvq03ZpclHQvgCQihBVz/view?usp=sharing">
               RESUME
