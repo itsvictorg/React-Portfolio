92ea14c41a5600c0deccda01665e8585df09f503
unknown <itsvictorg@gmail.com>
2023-05-23 19:42:07 -0500

    changed about me

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -53,7 +53,7 @@ export default function AboutMe() {
       <MediaQuery query="(min-width: 769px)">
         <Container fluid style={containerStyle} className="headline-faster">
           <p style={fontStyle}>
-          I'm Victor Gonzalez, an experienced web developer venturing into Python for machine learning and AI. With a strong foundation in web development, I thrive on creative problem-solving through coding. Beyond coding, I engage in diverse interests, seek continuous learning, and aspire to contribute to innovative projects in the dynamic fields of Python, machine learning, and AI. My passion for these domains, combined with my expertise in web development, drives me to make a meaningful impact in this exciting technological landscape.
+          I'm Victor Gonzalez, a full-stack engineer with a keen interest in exploring the fascinating realms of Python, machine learning, and AI. With a solid foundation in web development, I excel in crafting innovative solutions through code. While my primary focus lies in full-stack engineering, I am also enthusiastically immersing myself in Python to delve into the vast possibilities of machine learning and AI. By combining my expertise in web development with my burgeoning skills in Python, I aspire to contribute to cutting-edge projects in this ever-evolving technological landscape. My insatiable passion for these fields drives me to continually learn and make a meaningful impact in the world of full-stack engineering, Python, machine learning, and AI.
           </p>
           <div>
             <Button
